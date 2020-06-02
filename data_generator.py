18b8d2dc5c2f869c8faf493f2ca27a44b26c647d
Hades <hades@Hadess-MacBook-Pro.local>
2020-06-02 14:11:17 +0800

    model dictionary dumped, mean and std for x generation process dumped

--- data_generator.py
+++ data_generator.py
@@ -14,7 +14,6 @@ class SyntheticDataset:
             num_classes=2,
             seed=931231,
             num_dim=NUM_DIM,
-            prob_clusters=[1.0],
             n_parties = 2,
             x_level_noise = 1):
 
@@ -22,42 +21,29 @@ class SyntheticDataset:
         np.random.seed(seed)
         self.num_classes = num_classes
         self.num_dim = num_dim
-        self.num_clusters = len(prob_clusters)
-        self.prob_clusters = prob_clusters
-        self.side_info_dim = self.num_clusters
         self.n_parties = n_parties
         self.x_level_noise = x_level_noise
         self.Q = np.random.normal(
-            loc=0.0, scale=1.0, size=(self.num_dim + 1, self.num_classes, self.side_info_dim))
+            loc=0.0, scale=1.0, size=(self.num_dim + 1, self.num_classes))
 
         self.Sigma = np.zeros((self.num_dim, self.num_dim))
         for i in range(self.num_dim):
             self.Sigma[i, i] = (i + 1)**(-1.2)
 
-        self.means = self._generate_clusters()
 
 
-    def get_tasks(self, num_samples, weights, noises, loc_list):
+    def get_tasks(self, num_samples, weights, noises, loc_list, perturb_list):
 
         datasets = {}
+        model_weight_dic = {}
 
-
-        cluster_idx = np.random.choice(
-            range(self.num_clusters), size=None, replace=True, p=self.prob_clusters)
-
-        for i, (s, w, n, loc) in enumerate(zip(num_samples, weights, noises, loc_list)):
+        for i, (s, w, n, loc, perturb) in enumerate(zip(num_samples, weights, noises, loc_list, perturb_list)):
             print('current party is number. %s' %i)
-            new_task = self._generate_task(self.means[cluster_idx], cluster_idx, s, n, loc, w)
+            new_task, w = self._generate_task(s, n, loc, w, perturb)
             datasets[str(i)] = new_task
-        return datasets
-
-    def _generate_clusters(self):
-        means = []
-        for i in range(self.num_clusters):
-            loc = np.random.normal(loc=0, scale=1., size=None)
-            mu = np.random.normal(loc=loc, scale=1., size=self.side_info_dim)
-            means.append(mu)
-        return means
+            model_weight_dic[str(i)] = w.tolist()
+        model_weight_dic['global'] = self.Q.tolist()
+        return datasets, model_weight_dic
 
     def _generate_x(self, loc, num_samples):
         # loc = np.random.normal(loc=B, scale=1.0, size=self.num_dim)
@@ -67,13 +53,11 @@ class SyntheticDataset:
 
         return samples
 
-    def _generate_y(self, x, model_info, noise):
+    def _generate_y(self, x, noise, w):
 
 
         if self.x_level_noise == 1:
             x = x + np.random.normal(loc=0, scale=0.1, size=x.shape) * noise
-        
-        w = np.matmul(self.Q, model_info)
         
         num_samples = x.shape[0]
 
@@ -85,9 +69,9 @@ class SyntheticDataset:
             prob = prob + noise * np.random.normal(loc=0., scale=0.1, size=(num_samples, self.num_classes), axis=1)
                 
         y = np.argmax(prob, axis=1)
-        return y, w, model_info
+        return y
 
-    def _generate_task(self, cluster_mean, cluster_id, num_samples, noise, loc, label_weights):
+    def _generate_task(self, num_samples, noise, loc, label_weights, perturb):
 
         print('in _generate_task')
         num_labels = [math.floor(num_samples * i) for i in label_weights]
@@ -95,10 +79,9 @@ class SyntheticDataset:
         left_over = num_samples - sum(num_labels)
         num_labels[-1] += left_over
 
-        model_info = np.random.normal(loc=cluster_mean, scale=0.1, size=cluster_mean.shape)
-        
+        local_Q = self.Q + perturb
 
-        x, y=self._get_sets(num_labels, model_info, num_samples, noise, loc, [], [], 0)
+        x, y=self._get_sets(num_labels, num_samples, noise, loc, local_Q,[], [], 0)
 
         print('expected label distribution is %s' %num_labels)
         sorted_counts = self._count_helper(y, num_labels)
@@ -111,7 +94,7 @@ class SyntheticDataset:
 
         print('current potion after trimming step %s' %sorted_counts)
 
-        return {'x': final_x, 'y': final_y}
+        return {'x': final_x, 'y': final_y}, local_Q
 
     def _count_helper(self, y, num_labels):
 
@@ -140,23 +123,23 @@ class SyntheticDataset:
         return final_x, final_y
 
 
-    def _get_sets(self, num_labels, model_info, num_samples, noise, loc, full_x=[], full_y=[], count = 0):
+    def _get_sets(self, num_labels, num_samples, noise, loc, w, full_x=[], full_y=[], count = 0):
 
-        x, y = self._generate_trialset(model_info, num_samples, noise, loc)
+        x, y = self._generate_trialset(num_samples, noise, loc, w)
         full_x.extend(x)
         full_y.extend(y) 
         count +=1
         sorted_counts = self._count_helper(full_y, num_labels)
         for expect, reality in zip(num_labels, sorted_counts):
             if expect > reality:
-                x, y = self._get_sets(num_labels, model_info, num_samples, noise, loc, full_x, full_y, count)
+                x, y = self._get_sets(num_labels, num_samples, noise, loc, w, full_x, full_y, count)
         return full_x, full_y
 
 
 
-    def _generate_trialset(self, model_info, num_samples, noise, loc):
+    def _generate_trialset(self,num_samples, noise, loc, w):
         x = self._generate_x(loc, num_samples)
-        y, w, model_info = self._generate_y(x, model_info, noise)
+        y = self._generate_y(x, noise, w)
         x = x[:, 1:]
 
         return x, y
