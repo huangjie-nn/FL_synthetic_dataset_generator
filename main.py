f8e1df62c2bbd9137ca01f339b79d8857239005b
Hades <hades@Hadess-MacBook-Pro.local>
2020-06-04 11:03:39 +0800

    added in comment for each section for readability

--- main.py
+++ main.py
@@ -60,7 +60,13 @@ with open(base_path + "/params.json") as json_file:
 		data = json.load(json_file)
 
 print('Generating dataset')
+
+############################
+# getting params
+############################
+
 np.random.seed(data['meta']['seed'])
+
 num_samples = get_num_samples(
 	data['sample_size']['data_portion'],
 	data['meta']['n_classes'],
@@ -98,6 +104,9 @@ perturb_list = get_perturbation(
 	data['meta']['n_classes']
 )
 
+############################
+# generating datasets
+############################
 g = generator.SyntheticDataset(
 	num_classes=data['meta']['n_classes'],
 	num_dim=data['meta']['n_features'],
@@ -108,7 +117,6 @@ g = generator.SyntheticDataset(
 
 datasets, model_weight_dic = g.get_tasks(num_samples, weights, noises, loc_list, perturb_list)
 
-print('test test %s' %([0] * data['meta']['n_parties']))
 testset, _ = g.get_tasks(
 	([data['meta']['testset_size_per_party']]*data['meta']['n_parties']),
 	test_weights,
@@ -116,12 +124,20 @@ testset, _ = g.get_tasks(
 	loc_list,
 	perturb_list)
 
-
-user_data = to_format(datasets)
-test_data = test_to_format(testset)
 x_stats = get_x_stats(g, loc_list)
 
+############################
+# formatting
+############################
+user_data = to_format(datasets)
+test_data = test_to_format(testset)
+
+############################
+# saving 
+############################
 save_json(base_path, 'data.json', user_data)
 save_json(base_path, 'test_data.json', test_data)
 save_json(base_path,'x_stats.json',x_stats)
 save_json(base_path,'Q_dict.json', model_weight_dic)
+
+print('data generation done')
